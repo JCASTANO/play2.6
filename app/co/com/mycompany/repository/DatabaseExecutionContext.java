45feafb9df881f8ad642b48df3b7de45bdfc6bd1
Juan Esteban <juan.castano@ceiba.com.co>
2017-10-15 16:42:21 -0500

    GRAL:Ajuste

--- app/co/com/mycompany/repository/DatabaseExecutionContext.java
+++ app/co/com/mycompany/repository/DatabaseExecutionContext.java
@@ -1,37 +1,19 @@
 package co.com.mycompany.repository;
 
-import akka.actor.ActorSystem;
-import scala.concurrent.ExecutionContext;
-import scala.concurrent.ExecutionContextExecutor;
-
 import javax.inject.Inject;
+
+import akka.actor.ActorSystem;
+import play.libs.concurrent.CustomExecutionContext;
 
 /**
  * Custom execution context wired to "database.dispatcher" thread pool
  */
-public class DatabaseExecutionContext implements ExecutionContextExecutor {
-    private final ExecutionContext executionContext;
+public class DatabaseExecutionContext extends CustomExecutionContext {
 
-    private static final String name = "database.dispatcher";
+    private static final String NAME = "database.dispatcher";
 
     @Inject
     public DatabaseExecutionContext(ActorSystem actorSystem) {
-        this.executionContext = actorSystem.dispatchers().lookup(name);
-    }
-
-    @SuppressWarnings("deprecation")
-	@Override
-    public ExecutionContext prepare() {
-        return executionContext.prepare();
-    }
-
-    @Override
-    public void execute(Runnable command) {
-        executionContext.execute(command);
-    }
-
-    @Override
-    public void reportFailure(Throwable cause) {
-        executionContext.reportFailure(cause);
+        super(actorSystem, NAME);
     }
 }
